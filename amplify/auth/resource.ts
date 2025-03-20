fb2f42b21adba97a2d72e710e0b2ddf0e7588468
cathaleia <eficiencia.desarrollo@copowe.com.co>
2025-03-20 15:17:02 -0500

    corriegiendo rutas del lado del servidor

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -8,8 +8,8 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
     externalProviders: {
-      callbackUrls: ["http://localhost:3000/api/auth/sign-in-callback"],
-      logoutUrls: ["http://localhost:3000/api/auth/sign-out-callback"],
+      callbackUrls: ["https://main.d1csidv6wig1cv.amplifyapp.com/api/auth/sign-in-callback"],
+      logoutUrls: ["https://main.d1csidv6wig1cv.amplifyapp.com/api/auth/sign-out-callback"],
     },
   },
 
